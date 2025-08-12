553b42ca0c48380e1bb22049c9a6b532ae049e5c
LTDat5 <88955236+LTDat03@users.noreply.github.com>
2025-08-12 11:51:18 +0700

    add backend

---
+++ reuse-hub-backend/src/main/java/vn/tphcm/reusehubbackend/ReuseHubBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.tphcm.reusehubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuseHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReuseHubBackendApplication.class, args);
+    }
+
+}
